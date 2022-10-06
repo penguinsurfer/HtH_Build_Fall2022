cb46578352516c5c2d76a0d31bf7e8dfe352f3b0
GeorgeWindow <78511654+penguinsurfer@users.noreply.github.com>
2022-10-06 23:47:06 +0000

    Initial commit

---
+++ about_me.py
@@ -0,0 +1,7 @@
+name = "george"
+nickname = "jorge"
+age = 17
+have_used_python_before = True
+hobbies_list = ['games', 'working out', 'watching anime']
+dict = {'spirited away', 'chicken', 'video games'}
+all_vars = dict(vars())
