74f1ef0c485c1f2ed7349de3ef9a08f986ddf627
penguinsurfer <george.kong40@gmail.com>
2022-10-06 18:58:15 -0700

    dadad a

---
+++ about_me.py
@@ -0,0 +1,14 @@
+name = "george"
+nickname = "jorge"
+age = 17
+have_used_python_before = True
+hobbies_list = ['games', 'working out', 'watching anime']
+dictionary = {1: 'spirited away', 2: 'chicken', 3: 'video games'}
+all_vars = dict(vars())
+
+print(name)
+print(nickname)
+print(age)
+print(have_used_python_before)
+print(hobbies_list)
+print(dictionary)
