be37c7cc59b7f0b1a744ac2463d9d1c70bde0a6b
penguinsurfer <george.kong40@gmail.com>
2022-10-06 18:24:02 -0700

    ghyawdad

--- about_me.py
+++
@@ -1,7 +0,0 @@
-name = "george"
-nickname = "jorge"
-age = 17
-have_used_python_before = True
-hobbies_list = ['games', 'working out', 'watching anime']
-dictionary = {'spirited away', 'chicken', 'video games'}
-all_vars = dict(vars())
